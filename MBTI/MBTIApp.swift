e1af577dae92dd9bd8dc56f8d6fa3dc09939478e
minsu.medison@gmail.com <minsu.medison@gmail.com>
2024-11-05 22:35:06 +0900

    Initial Commit

---
+++ MBTI/MBTIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MBTIApp.swift
+//  MBTI
+//
+//  Created by Minsu Han on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MBTIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MBTITests/MBTITests.swift
@@ -0,0 +1,17 @@
+//
+//  MBTITests.swift
+//  MBTITests
+//
+//  Created by Minsu Han on 11/5/24.
+//
+
+import Testing
+@testable import MBTI
+
+struct MBTITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
